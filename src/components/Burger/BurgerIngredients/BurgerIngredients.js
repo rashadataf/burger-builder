a04916b9d823620411c26d3f98e32339e21f605e
Rashad Ataf <rashadattaf@gmail.com>
2020-04-25 15:12:42 +0300

    creating the Component of the burger's ingredients with styles

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,35 @@
+// This component responsible for adding the ingredients to the burger
+// which mean here the salad, cheese, ... will be added to the burger
+import React from "react";
+import classes from "./BurgerIngredients.module.css";
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+    switch (props.type) {
+        case('bread-bottom'):
+            ingredient = <div className={classes.BreadBottom}></div>;
+            break;
+        case('bread-top'):
+            ingredient = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
+            );
+            break;
+        case('meat'):
+            ingredient = <div className={classes.Meat}></div>;
+        break;
+        case('cheese'):
+            ingredient = <div className={classes.Cheese}></div>;
+            break;
+        case('bacon'):
+            ingredient = <div className={classes.Bacon}></div>;
+            break;
+        case('salad'):
+            ingredient = <div className={classes.Salad}></div>;
+            break;
+    }
+};
+
+export default burgerIngredient;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,12 @@
 
 import React from "react";
 import Aux from "../../hoc/Aux";
+import styles from "./Layout.module.css";
 
 const layout = (props)=> (
     <Aux>
         <div>Here the Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </Aux>
